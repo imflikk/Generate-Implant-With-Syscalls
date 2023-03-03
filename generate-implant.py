9755b698d21bd8f9103f1c7081e34e8e19d192d6
imflikk <flikkme@gmail.com>
2023-03-03 15:10:50 -0800

    Added clearing of src folder pre-compile to avoid issues with cloned template file

--- generate-implant.py
+++ generate-implant.py
@@ -48,6 +48,18 @@ def main():
 	# Make directory structure and write source files
 	if not os.path.exists("src"):
 		os.mkdir("src")
+	else:
+		print("[*] Clearing src folder to avoid compilation issues...")
+		folder = 'src\\'
+		for filename in os.listdir(folder):
+		    file_path = os.path.join(folder, filename)
+		    try:
+		        if os.path.isfile(file_path) or os.path.islink(file_path):
+		            os.unlink(file_path)
+		        elif os.path.isdir(file_path):
+		            shutil.rmtree(file_path)
+		    except Exception as e:
+		        print('Failed to delete %s. Reason: %s' % (file_path, e))
 
 	with open("src\\compile.bat", "w") as f:
 		f.write(b64.b64decode(b64_compile_bat).decode())
